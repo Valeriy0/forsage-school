84ea040429a94e9dc68ca3a82ffde1b1a6dae34e
Sergo588 <mazmanyan_1990@mail.ru>
2023-05-22 13:38:19 +0300

    update

--- src/features/Earning/index.jsx
+++ src/features/Earning/index.jsx
@@ -3,28 +3,37 @@ import react from "react";
 export const Earning = () => {
 
   return (
-        <div className="flex flex-col items-center space-y-14 pt-[50px] w-full">
-            <span className="text-4xl font-medium ">Let's recap</span>
+        <div className="flex flex-col items-center space-y-14 pt-[50px] w-full sm:space-y-0">
+            <div className="flex justify-center sm:w-full ">
+                <img className="absolute h-[23vh]  left-0 hidden sm:flex" src="/img/earning/gamepadMob.png" />
+                <span className="text-4xl font-medium sm:text-3xl sm:mt-[30px] sm:font-bold">Let's recap</span>
+                <img className=" absolute h-[18vh]  right-0 hidden sm:flex " src="/img/earning/secondPicMob.png" />
+            </div>
             <div className=" flex items-start justify-between w-full ">
-                <img className=" w-[55vh] left-0" src="/img/earning/gamepad.png" />
-                <div className="bg-[#14191B] rounded-[30px] border border-1 py-11 px-[50px] flex flex-col items-center space-y-7 mt-[50px]">
-                    <span className="text-2xl font-bold">How do you earn at FORSAGE?</span>
-                    <div className="flex flex-col items-start space-y-4 opacity-50">
-                        <div className="flex items-center justify-center space-x-5">
-                            <div className="rounded-full border border-1 border-white p-2 "></div>
-                            <span>Activate levels in programs and wait for passive income.</span>
-                        </div>
-                        <div className="flex items-center justify-center space-x-5">
-                            <div className="rounded-full border border-1 border-white p-2 "></div>
-                            <span>Invite partners to your team and activate levels and <br/> programs together.</span>
-                        </div>
-                        <div className="flex items-center justify-center space-x-5">
-                            <div className="rounded-full border border-1 border-white p-2 "></div>
-                            <span>Wait for income from your upline partner.</span>
+                <img className=" w-[55vh] left-0 sm:hidden" src="/img/earning/gamepad.png" />
+                <div className=" flex flex-col relative items-center">
+                    <div className="bg-[#14191B] rounded-[30px] border border-1 py-11 px-[50px] flex flex-col items-center space-y-7 mt-[50px] relative sm:top-[50px] sm:px-5 sm:py-5">
+                        <div className="w-[100px] h-[100px] bg-[#14191B] rounded-full absolute top-[-65px] border"></div>
+                        <span className="text-2xl font-bold text-center">How do you earn at FORSAGE?</span>
+                        <div className="flex flex-col items-start space-y-4 opacity-50">
+                            <div className="flex items-center justify-center space-x-5">
+                                <div className="rounded-full border border-1 border-white p-2 "></div>
+                                <span>Activate levels in programs and wait for passive income.</span>
+                            </div>
+                            <div className="flex items-center justify-center space-x-5">
+                                <div className="rounded-full border border-1 border-white p-2 "></div>
+                                <span>Invite partners to your team and activate levels and <br/> programs together.</span>
+                            </div>
+                            <div className="flex items-center justify-center space-x-5">
+                                <div className="rounded-full border border-1 border-white p-2 "></div>
+                                <span>Wait for income from your upline partner.</span>
+                            </div>
                         </div>
                     </div>
+                    <div className="w-11/12 h-[200px] border rounded-[30px] absolute top-[180px] sm:top-[285px] z-[-10] px-20 bg-[#14191B]"></div>
+                    <div className="w-9/12 h-[200px] border rounded-[30px] absolute top-[200px] sm:top-[305px] z-[-15] px-20 bg-[#14191B]"></div>
                 </div>
-                <img className="w-[55vh] right-0" src="/img/earning/secondPic.png" />
+                <img className="w-[55vh] right-0 sm:hidden" src="/img/earning/secondPic.png" />
             </div>
         </div>
   )
--- src/features/InstallWallet/index.jsx
+++ src/features/InstallWallet/index.jsx
@@ -3,12 +3,12 @@ import react from "react";
 export const InstallWallet = () => {
 
   return (
-        <div className="flex flex-col items-center justify-center mt-[200px]">
+        <div className="flex flex-col items-center justify-center pt-[200px] w-full">
             <div className=" flex flex-col items-center justify-center space-y-7">
-                <span className="text-5xl text-center font-bold">Installing a wallet on your <br/> smartphone or computer</span>
-                <span className="opacity-50 text-center">To sign up with Forsage, you need a wallet. An app gives you access to registration and personal <br />dashboard, as well as to the crypto that you earn.</span>
+                <span className="text-5xl text-center font-bold sm:text-4xl">Installing a wallet <br className="hidden sm:flex"/> on your <br className="sm:hidden"/> smartphone or computer</span>
+                <span className="opacity-50 text-center text-xl sm:text-base">To sign up with Forsage, you need a wallet. An app gives you access to registration and personal <br />dashboard, as well as to the crypto that you earn.</span>
             </div>
-            <div className="mt-[70px] flex space-x-12">
+            <div className="mt-[70px] flex space-x-12 sm:flex-col sm:space-x-0 sm:w-full sm:space-y-9">
                 <div className="bg-[#14191B] py-5 px-18 rounded-[25px]  flex flex-col items-center space-y-6">
                     <span className="text-center">Installing <span className="text-[#2980FE]"> TokenPoket </span> <br /> Wallet</span>
                     <img className="w-[200px] h-[200px]" src="/img/wallets/TP.png"/>
