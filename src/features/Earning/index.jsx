17cf79a18a6bef72cf1f912fccf27396c86635fa
«Valeriy0» <v.kistrinov@yandex.ru>
2023-05-23 13:50:15 +0300

    upd

--- src/features/Earning/index.jsx
+++ src/features/Earning/index.jsx
@@ -6,6 +6,11 @@ export const Earning = ({ id, title, testContent }) => {
     const [currentAnswer, setCurrentAnswer] = useState(null);
     const isCurrentTrue = !!testContent[currentAnswer]?.isTrue ? testContent[currentAnswer]?.isTrue : false;
 
+    const backgroundList = {
+        left: `/img/earning/${id}/left.png`,
+        right: `/img/earning/${id}/right.png`,
+    };
+
     const renderIcon = () => {
         if (!!testContent[currentAnswer]) {
             if (testContent[currentAnswer]?.isTrue) {
@@ -18,14 +23,14 @@ export const Earning = ({ id, title, testContent }) => {
     }
 
   return (
-        <div id={`recap-${id}`} className="flex flex-col justify-center items-center space-y-14 pt-[50px] pb-[100px] w-full sm:space-y-0">
+        <div id={`recap-${id}`} className="relative flex flex-col justify-center items-center space-y-14 pt-[50px] pb-[100px] w-full sm:space-y-0">
             <div className="flex justify-center sm:w-full ">
-                <img className="absolute h-[23vh] left-0 hidden sm:flex" src="/img/earning/gamepadMob.png" />
+                <img className="absolute h-[23vh] -left-5 hidden sm:flex" src={backgroundList?.left} />
                 <span className="text-4xl font-medium sm:text-3xl sm:mt-[30px] sm:font-bold font-montserrat">Let's recap</span>
-                <img className=" absolute h-[18vh] right-0 hidden sm:flex " src="/img/earning/secondPicMob.png" />
+                <img className="absolute h-[18vh] -right-5 hidden sm:flex " src={backgroundList?.right} />
             </div>
             <div className=" flex items-start justify-center w-full sm:w-full ">
-                <img className="absolute w-[55vh] z-[-20] left-0 sm:hidden" src="/img/earning/gamepad.png" />
+                <img className="absolute w-[55vh] z-[-20] top-0 left-0 sm:hidden" src={backgroundList?.left} />
                 <div className=" flex flex-col relative items-center sm:w-full">
                     <div className={`${!!testContent[currentAnswer] ? isCurrentTrue ? 'button-gradient' : 'test-gradient-wrapper-err' : 'button-gradient'}  p-[1px] flex justify-center items-center mt-[50px] relative sm:top-[50px] rounded-[30px]`}>           
                         <div className="bg-[#14191B] rounded-[30px] py-11 px-[50px] flex flex-col items-center space-y-7  sm:px-5 sm:py-5 sm:w-full">
@@ -58,7 +63,7 @@ export const Earning = ({ id, title, testContent }) => {
                         <div className="w-full h-full rounded-[30px]  px-20 bg-[#14191B]"></div>
                     </div>
                 </div>
-                <img className="absolute w-[55vh] right-0 z-[-20] sm:hidden" src="/img/earning/secondPic.png" />
+                <img className="absolute w-[55vh] top-0 right-0 z-[-20] sm:hidden" src={backgroundList?.right} />
             </div>
         </div>
   )
