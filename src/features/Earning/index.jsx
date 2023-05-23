11e32625c863fddccdd66e001e9aab850cc915ce
«Valeriy0» <v.kistrinov@yandex.ru>
2023-05-23 11:32:17 +0300

    upd

--- src/features/Earning/index.jsx
+++ src/features/Earning/index.jsx
@@ -3,8 +3,6 @@ import react, { useState } from "react";
 
 export const Earning = () => {
 
-    const [currentAnswer, setCurrentAnswer] = useState();
-
     const testContent = [
         {
             title: 'Activate levels in programs and wait for passive income.',
@@ -18,7 +16,21 @@ export const Earning = () => {
             title: 'Wait for income from your upline partner.',
             isTrue: false,
         },
-    ]
+    ];
+
+    const [currentAnswer, setCurrentAnswer] = useState(null);
+    const isCurrentTrue = !!testContent[currentAnswer]?.isTrue ? testContent[currentAnswer]?.isTrue : false;
+
+    const renderIcon = () => {
+        if (!!testContent[currentAnswer]) {
+            if (testContent[currentAnswer]?.isTrue) {
+                return <img className="w-[40px]" src='/img/test/suc.png'/>
+            } else {
+                return <img className="w-[40px]" src='/img/test/err.png'/>
+            }
+        } 
+        return null
+    }
 
   return (
         <div className="flex flex-col items-center space-y-14 pt-[50px] w-full sm:space-y-0 sm:w-full">
@@ -30,12 +42,12 @@ export const Earning = () => {
             <div className=" flex items-start justify-between w-full sm:w-full ">
                 <img className=" w-[55vh] left-0 sm:hidden" src="/img/earning/gamepad.png" />
                 <div className=" flex flex-col relative items-center sm:w-full">
-                    <div className="button-gradient p-[1px] flex justify-center items-center mt-[50px] relative sm:top-[50px] rounded-[30px]">           
+                    <div className={`${!!testContent[currentAnswer] ? isCurrentTrue ? 'button-gradient' : 'test-gradient-wrapper-err' : 'button-gradient'}  p-[1px] flex justify-center items-center mt-[50px] relative sm:top-[50px] rounded-[30px]`}>           
                         <div className="bg-[#14191B] rounded-[30px] py-11 px-[50px] flex flex-col items-center space-y-7  sm:px-5 sm:py-5 sm:w-full">
                             
-                            <div className="w-[100px] h-[100px] rounded-full absolute top-[-65px] button-gradient p-[1px] flex justify-center items-center">
+                            <div className={`w-[100px] h-[100px] rounded-full absolute top-[-65px] p-[1px] flex justify-center items-center ${!!testContent[currentAnswer] ? isCurrentTrue ? 'button-gradient' : 'test-gradient-wrapper-err' : 'button-gradient'} `}>
                                 <div className="flex items-center justify-center w-full h-full bg-[#14191B] rounded-full">
-                                    {testContent[currentAnswer]?.isTrue ? 1 : 0}
+                                   {renderIcon()}
                                 </div>
                             </div>
 
@@ -47,17 +59,17 @@ export const Earning = () => {
                                     return (
                                         <div onClick={() => setCurrentAnswer(itemIndex)} className={`flex items-center justify-center space-x-5 ${isChecked ? isTrue ? 'test-gradient-suc' : 'test-gradient-err' : 'opacity-50'}`}>
                                             <input checked={isChecked} type="radio" id={`input-${itemIndex}`} className="rounded-full border border-1 border-white p-2" />
-                                            <label for={`input-${itemIndex}`}>Activate levels in programs and wait for passive income.</label>
+                                            <label for={`input-${itemIndex}`}>{item?.title}</label>
                                         </div>
                                     )
                                 })}
                             </div>
                         </div>
                     </div>
-                    <div className="opacity-50 h-[200px] w-full max-w-[500px] button-gradient p-[1px] flex justify-center items-center absolute top-[180px] sm:top-[10/12] z-[-10] rounded-[30px]"> 
+                    <div className={`opacity-50 h-[200px] w-full max-w-[500px] button-gradient p-[1px] flex justify-center items-center absolute top-[180px] sm:top-[10/12] z-[-10] rounded-[30px] ${!!testContent[currentAnswer] ? isCurrentTrue ? 'button-gradient' : 'test-gradient-wrapper-err' : 'button-gradient'}`}> 
                         <div className="w-full h-full rounded-[30px]  px-20 bg-[#14191B]"></div>
                     </div>
-                    <div className="opacity-20 h-[200px] w-full max-w-[450px] button-gradient p-[1px] flex justify-center items-center absolute top-[200px] sm:top-[10/12] z-[-15] rounded-[30px]"> 
+                    <div className={`opacity-20 h-[200px] w-full max-w-[450px] button-gradient p-[1px] flex justify-center items-center absolute top-[200px] sm:top-[10/12] z-[-15] rounded-[30px] ${!!testContent[currentAnswer] ? isCurrentTrue ? 'button-gradient' : 'test-gradient-wrapper-err' : 'button-gradient'}`}> 
                         <div className="w-full h-full rounded-[30px]  px-20 bg-[#14191B]"></div>
                     </div>
                 </div>
